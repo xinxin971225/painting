967e258e5839a58f8af36ace69399202396314c8
zhiyu <872906473@qq.com>
2020-03-06 18:45:49 +0800

    创建手机端服务

--- js/main.js
+++ js/main.js
@@ -26,16 +26,20 @@ let step = -1;
 
   //改变线条大小
   upLineWidth.onclick = function(){
+
     context.lineWidth=context.lineWidth+1
 
     useEraser = false
+    pen.classList.add('active')
     eraser.classList.remove('active')
     penSize.innerHTML = '当前粗细:'+context.lineWidth
   }
   downLineWidth.onclick = function(){
+
     context.lineWidth=context.lineWidth-1
     penSize.innerHTML = '当前粗细:'+context.lineWidth
     useEraser = false
+    pen.classList.add('active')
     eraser.classList.remove('active')
   }
 
@@ -49,17 +53,19 @@ let step = -1;
    }
    //声明
  var useEraser = false
+ var drawing = false
  eraser.onclick = function(){
   useEraser = true
   eraser.classList.add('active')
   pen.classList.remove('active')
  }
  pen.onclick = function(){
-  useEraser = false
-  pen.classList.add('active')
-  eraser.classList.remove('active')
+     useEraser = false
+        pen.classList.add('active')
+        eraser.classList.remove('active')
+
+ 
  }
- var drawing = false
 
 
 
@@ -68,20 +74,78 @@ let step = -1;
 context.fillStyle ='#' + jscolor;
 context.strokeStyle ='#' + jscolor;
 var nowcolor = document.querySelector('#nowcolor')
-console.log(nowcolor.style)
 nowcolor.style.color='#' + jscolor
 }
 
+//检测设备
+if(document.body.ontouchstart !== undefined){
+    //手指设备
+    canvas.ontouchstart= function(aaa) {
 
+        let x = aaa.touches[0].clientX;
+        let y = aaa.touches[0].clientY;
 
+            if(useEraser){
+                context.clearRect(x-10,y-10,20,20);
+                drawing = true
+               }else{
+                drawing = true;
+                drawcricle(x-0.5, y-0.5, 1);
+                lastPoint = {
+                 'x': x,
+                 'y': y
+               }
+             
+        }
+       
+      }
+     
+      //手指移动
+      canvas.ontouchmove = function(aaa) {
 
+        let x = aaa.touches[0].clientX;
+        let y = aaa.touches[0].clientY;
+        let newPoint = {
+          'x': x,
+          'y': y
+        }
 
+            if (useEraser) {
+                if(drawing){
+                 context.clearRect(x-10,y-10,20,20)
+                }
+              }else if(drawing){
+                context.beginPath();
+                context.moveTo(lastPoint.x, lastPoint.y);
+                context.lineTo(newPoint.x, newPoint.y);
+                context.stroke();
+                context.closePath();
+                lastPoint = newPoint;
+              }
 
-       //鼠标按下
- canvas.onmousedown = function(aaa) {
+    }
+      
+      
+      
+      //手指松开
+      canvas.ontouchend = function(aaa) {
+        drawing = false;
+        step++;
+        if (step < canvasHistory.length) {
+            canvasHistory.length = step; // 截断数组
+        }
+        canvasHistory.push(canvas.toDataURL()); // 添加新的绘制到历史记录
+      }
+     
+      
+
+}else{
+    //非触屏设备
+   //鼠标按下
+   canvas.onmousedown = function(aaa) {
    
-    var x = aaa.clientX;
-    var y = aaa.clientY;
+    let x = aaa.clientX;
+    let y = aaa.clientY;
     if(useEraser){
      context.clearRect(x-10,y-10,20,20);
      drawing = true
@@ -99,9 +163,9 @@ nowcolor.style.color='#' + jscolor
  
   //鼠标移动
   canvas.onmousemove = function(aaa) {
-    var x = aaa.clientX;
-    var y = aaa.clientY;
-    var newPoint = {
+    let x = aaa.clientX;
+    let y = aaa.clientY;
+    let newPoint = {
       'x': x,
       'y': y
     }
@@ -131,30 +195,40 @@ nowcolor.style.color='#' + jscolor
     canvasHistory.push(canvas.toDataURL()); // 添加新的绘制到历史记录
     
   }
- 
+
+}
+ // 撤销方法
+ canvasUndo.onclick = function() {
+    if (step >= 0) {
+      step--;
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      let canvasPic = new Image();
+      canvasPic.src = canvasHistory[step];
+      canvasPic.addEventListener('load', () => {
+          context.drawImage(canvasPic, 0, 0);
+      });
+    }
+  }
+  // 反撤销方法
+  canvasRedo.onclick = function() {
+    if (step < canvasHistory.length - 1) {
+      step++;
+      let canvasPic = new Image();
+      canvasPic.src = canvasHistory[step];
+      canvasPic.addEventListener('load', () => {
+          context.clearRect(0, 0, canvas.width, canvas.height);
+          context.drawImage(canvasPic, 0, 0);
+      });
+    }
+  }
+
+
+
+
+
+
+
+
+
  
 
-// 撤销方法
-canvasUndo.onclick = function() {
-  if (step >= 0) {
-    step--;
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    let canvasPic = new Image();
-    canvasPic.src = canvasHistory[step];
-    canvasPic.addEventListener('load', () => {
-        context.drawImage(canvasPic, 0, 0);
-    });
-  }
-}
-// 反撤销方法
-canvasRedo.onclick = function() {
-  if (step < canvasHistory.length - 1) {
-    step++;
-    let canvasPic = new Image();
-    canvasPic.src = canvasHistory[step];
-    canvasPic.addEventListener('load', () => {
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        context.drawImage(canvasPic, 0, 0);
-    });
-  }
-}
